201c8252d472b9716b2a303d2e85c61e3da48fea
abdelqayyim <abdelymaster@gmail.com>
2022-04-14 03:02:52 -0400

    Version 1.0
    Fixed Many Bugs, Yet to Implement the delete note button. Next Major Step is the Database

--- index.js
+++ index.js
@@ -1,57 +1,152 @@
-// import Prism from prismjs;
-// const Prism = require("prismjs");
-
-// const Prism = require("./prism-dark");
-
-//TODO: When the languages are added, add them in the data right away
-//TASK: Create an add/delete button for the languages (Don't forget to delete from database too)
-//TASK: create a foreach look to write the notes when the language is pressed
+let body = document.querySelector("body");
+let languagePicked; //this is a global variable that keeps track of the language the user is working on
+// ----------------------------------- PART 1 -----------------------------
+let menu = document.querySelector(".languages");
 let options = document.querySelector(".options");
-
-let dropDown = document.querySelector(".dropInfo");
-let dropArrow = document.querySelector(".btn-dropdown");
-
+let languages = document.querySelectorAll(".lang"); //the languages buttons
+let dropArrow = document.querySelector(".btn-dropdown"); //the options button (add and delete language)
+let dropDown = document.querySelector(".dropInfo"); //the two options (add and delete)
 let addLanguageOption = document.querySelector(".add-languageOption");
+let deleteLanguageOption = document.querySelector(".delete-languagegOption");
+let arrowDown = document.querySelector("i");
+let addNoteButton = document.querySelector(".addNoteBtn"); //this is the button that allows the use to add a note
+let newNoteBtn = document.querySelector(".createNoteBtn");
+let newNoteTitle = document.querySelector(".titleInput");
+let newNoteDescription = document.querySelector(".descriptionInput");
+
+//           ---------- FUNCTION CALLS -----------------------
+addLanguageOption.addEventListener("click", showAddLanguage);
+deleteLanguageOption.addEventListener("click", showDeleteLanguage);
+languages.forEach((lang) => { lang.addEventListener("click", (e) => { showNotes(e, lang) }) });
+dropArrow.addEventListener("click", dropOptions);
+addNoteButton.addEventListener("click", openNotePopUp);
+newNoteBtn.addEventListener("click", (e) => { createNewNote(e, languagePicked) });
+// ----------------------------------- PART 2 -----------------------------
+let notesSection = document.querySelector(".notes-section"); //this is the section where the notes are
+let notes = document.querySelectorAll(".note"); //the notes in the note section
+
+//           ---------- FUNCTION CALLS -----------------------
+notes.forEach((note) => note.addEventListener("click", function (e) { showNoteDetail(e, languagePicked); }));
+// ----------------------------------- PART 3 -----------------------------
+let noteDetail = document.querySelector(".note-detail"); //this is the pop up after you press on a note
+let noteTitle = document.querySelector(".note-title");
+let addNoteBtn = document.querySelector(".addNote");
+let addCodeBtn = document.querySelector(".addCode");
+let postBtn = document.querySelector(".save");
+let closeBtn = document.querySelector(".close-btn");
+
+//           ---------- FUNCTION CALLS -----------------------
+addNoteBtn.addEventListener("click", addNote);
+addCodeBtn.addEventListener("click", function (e) { addCode(e, languagePicked)});
+closeBtn.addEventListener("click", closeBtnFunction)
+
+// ----------------------------------- PART 4 -----------------------------
 let addLanguagePopUp = document.querySelector(".add-language"); //this is the div
 let addLanguageInput = document.querySelector(".add-languageInput"); //the input
 let addLanguageBtn = document.querySelector(".add-languageBtn"); //the button
 
-let deleteLanguageOption = document.querySelector(".delete-languagegOption");
 let deleteLanguagePopUp = document.querySelector(".delete-language"); //this is the div
 let deleteLanguageInput = document.querySelector(".delete-languageInput"); //the input
 let deleteLanguageBtn = document.querySelector(".delete-languageBtn"); //the button
 
-let arrowDown = document.querySelector("i");
-let languages = document.querySelectorAll(".lang");
-let menu = document.querySelector(".languages");
-let body = document.querySelector("body");
+let createNotePop = document.querySelector(".create-note");
+let createNoteBtn = document.querySelector(".createNoteBtn");//TASK: create the note when the button is clicked, check that title is not empty
 
-let notesSection = document.querySelector(".notes-section");
-let notes = document.querySelectorAll(".note");
-let noteDetail = document.querySelector(".note-detail");
-let closeBtn = document.querySelector(".close-btn");
-let editNoteBtn = document.querySelectorAll(".fa-edit");
-
+//           ---------- FUNCTION CALLS -----------------------
+addLanguageBtn.addEventListener("click", (e) => { addLanguageFunction(e) });
+deleteLanguageBtn.addEventListener("click", (e) => { deleteLanguageFunction(e) });
+// ----------------------------------- PART 5 -----------------------------
 let overlay = document.querySelector(".overlay");
 let lightOverlay = document.querySelector(".light-overlay");
-let postBtn = document.querySelector(".save");
-let inputText = document.querySelector("#text");
-let noteTitle = document.querySelector(".note-title");
 
-let languagePicked; 
-/*********************  EDITOR *****************************/
-// var editor = ace.edit("editor");
-// editor.setTheme("ace/theme/monokai");
-// editor.getSession().setMode("ace/mode/javascript");
-/*********************************************************/
+//           ---------- FUNCTION CALLS -----------------------
+lightOverlay.addEventListener("click", lightOverlayFunction);
+// -------------------------------------------------------------------------------------------------------
 
-closeBtn.addEventListener("click", function (e) {
+
+
+function closePopUps(e) {
+    if (e.target == overlay) {
+        overlay.classList.remove("active");
+        addLanguagePopUp.classList.add("hidden");
+        deleteLanguagePopUp.classList.add("hidden");
+        document.removeEventListener("click", closePopUps);
+    }
+}
+function closeBtnFunction() {
     noteDetail.classList.remove("active");
     overlay.classList.remove("active");
-})
-addLanguageBtn.addEventListener("click", (e) => {
-    console.log(this);
-    e.preventDefault();
+}
+function dropOptions() {
+    dropDown.classList.add("drop");
+    
+    if (arrowDown.classList[1] === "fa-angle-down") {
+        arrowDown.classList.remove("fa-angle-down");
+        arrowDown.classList.add("fa-angle-up");
+        lightOverlay.classList.add("active");
+    }
+    else {
+        arrowDown.classList.add("fa-angle-down");
+        arrowDown.classList.remove("fa-angle-up");
+        dropDown.classList.remove("drop");
+    }
+}
+function addNote(){
+    let noteSection = document.querySelector(".note-ul");
+    let note = document.createElement("li");
+    note.classList.add("bulletPoint-list");
+    note.innerHTML = `<p class="bullet-point"<span role="textbox" contenteditable> Add Note</span></p>`;
+    noteSection.appendChild(note);
+    editNoteBtn = document.querySelectorAll(".fa-edit");
+    editNoteBtn.forEach((btn) => btn.addEventListener("click", editNote));
+}
+function addCode(event, language){
+    let noteSection = document.querySelector(".note-ul");
+    console.log(languagePicked);
+    let newNote = noteSection.childNodes[1];
+    let newD = document.createElement("div");
+    newD.innerHTML = `<pre class="line-numbers">
+    <code class="language-${language}" contenteditable>Add ${language} code here</code>
+</pre>`;
+    noteSection.appendChild(newD);
+    Prism.highlightAll();
+}
+function showNotes(event, language) {
+    languagePicked = language.classList[2].toLowerCase();
+    body.style.justifyContent = "start";
+    addNoteButton.classList.remove("hidden");
+    notesSection.classList.remove("hidden");
+    menu.classList.add("clicked");
+    noteTitle.innerText = `The Language is ${languagePicked}`;
+    console.log(languagePicked);
+    //TODO: Do not forget to change the content when the language is picked
+}
+function openNotePopUp() {
+    createNotePop.classList.remove("hidden");
+    dropArrow.style.zIndex = 0;
+    lightOverlay.classList.add("active");
+    lightOverlay.style.backgroundColor = "grey";
+    lightOverlay.style.opacity = 0.8;
+}
+function showAddLanguage() {
+    overlay.classList.add("active");
+    lightOverlay.classList.remove("active");
+    addLanguagePopUp.classList.remove("hidden");
+    setTimeout(() => {document.addEventListener("click",closePopUps)},500)
+}
+function showDeleteLanguage() {
+    overlay.classList.add("active");
+    lightOverlay.classList.remove("active");
+    deleteLanguagePopUp.classList.remove("hidden");
+    setTimeout(() => {document.addEventListener("click",closePopUps)},500)
+}
+function showNoteDetail(event, language) { //This is the note detail pop up, to edit
+    console.log("Pressed");
+    noteDetail.classList.add("active");
+    overlay.classList.add("active");
+}
+function addLanguageFunction(event) {
+    event.preventDefault();
     if (addLanguageInput.value.trim().length !== 0) {
         let languageName = addLanguageInput.value.trim();
         let newLanguage = document.createElement("button");
@@ -69,9 +164,9 @@ addLanguageBtn.addEventListener("click", (e) => {
     else { //if it is empty ***no words in the input field***
         
     }
-})
-deleteLanguageBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+}
+function deleteLanguageFunction(event){
+    event.preventDefault();
     if (deleteLanguageInput.value.trim().length !== 0) {
         let languageToDelete = deleteLanguageInput.value.trim().toLowerCase();
         options.removeChild(document.querySelector(`.${languageToDelete}`));
@@ -91,117 +186,34 @@ deleteLanguageBtn.addEventListener("click", (e) => {
     else { //if it is empty ***no words in the input field***
         
     }
-})
-
-dropArrow.addEventListener("click", function (e) {
-    dropDown.classList.add("drop");
-    
-    if (arrowDown.classList[1] === "fa-angle-down") {
-        arrowDown.classList.remove("fa-angle-down");
-        arrowDown.classList.add("fa-angle-up");
-        lightOverlay.classList.add("active");
-    }
-    else {
-        arrowDown.classList.add("fa-angle-down");
-        arrowDown.classList.remove("fa-angle-up");
-        dropDown.classList.remove("drop");
-    }
-})
-lightOverlay.addEventListener("click", () => {
-    dropDown.classList.toggle("drop");
+}
+function lightOverlayFunction() {
+    dropDown.classList.remove("drop");
     arrowDown.classList.add("fa-angle-down");
     arrowDown.classList.remove("fa-angle-up");
     lightOverlay.classList.remove("active");
-})
-function closePopUps(e) {
-    if (e.target == overlay) {
-        overlay.classList.remove("active");
-        addLanguagePopUp.classList.add("hidden");
-        deleteLanguagePopUp.classList.add("hidden");
-        document.removeEventListener("click", closePopUps);
+    createNotePop.classList.add("hidden");
+    dropArrow.style.zIndex = 10;
+    lightOverlay.style.backgroundColor = "white";
+    lightOverlay.style.opacity = 0;
+}
+let okay = {}
+function createNewNote(event, language) {
+    console.log(language);
+    if (newNoteTitle.value.trim() != "") {//if the title is  not empty
+        let div = document.createElement("div");
+        div.classList.add("note");
+        div.innerHTML = `<div class="note-title">
+        ${newNoteTitle.value.trim()}
+    </div>
+    <div class="note-description">
+        ${newNoteDescription.value.trim()}
+    </div>`;
+        notesSection.insertAdjacentElement("beforeend", div);
+        lightOverlayFunction();//close the popUp
+        okay[language] = {};
+        okay[language]["Title"] = newNoteTitle.value;
+        okay[language]["Description"] = newNoteDescription.value;
+        console.log(okay);
     }
-}
-addLanguageOption.addEventListener("click", showAddLanguage);
-deleteLanguageOption.addEventListener("click", showDeleteLanguage);
-
-languages.forEach((lang) => { 
-    lang.addEventListener("click", (e) => {
-        body.style.justifyContent = "start";
-        notesSection.classList.remove("hidden");
-        menu.classList.add("clicked");
-        languagePicked = lang.classList[2];
-        //TODO: Do not forget to change the content when the language is picked
-    })
-});
-
-notes.forEach((note) => note.addEventListener("click", function (e) {
-    noteDetail.classList.add("active");
-    overlay.classList.add("active");
-}))
-
-postBtn.addEventListener("click", (e) => {
-    console.log(languagePicked);
-    let title = noteTitle.innerText;
-    LANGUAGES.Languages[languagePicked][title] = inputText.value;
-    console.log(LANGUAGES);
-})
-
-// function displayNote(language, noteTitle) {
-//     //TASK: change the view from "insert" to view mode. So as if it was a note
-//     //TASK: change button from "POST", to "EDIT" now and add functionalities
-// }
-let addNoteBtn = document.querySelector(".addNote");
-let addCodeBtn = document.querySelector(".addCode");
-addNoteBtn.addEventListener("click", addNote);
-addCodeBtn.addEventListener("click", addCode);
-function addNote(){
-    let noteSection = document.querySelector(".note-ul");
-    let note = document.createElement("li");
-    note.classList.add("bulletPoint-list");
-    note.innerHTML = `<p class="bullet-point"<span role="textbox" contenteditable><i class="fa fa-edit"></i> Add Note</span></p>`;
-    noteSection.appendChild(note);
-    editNoteBtn = document.querySelectorAll(".fa-edit");
-    editNoteBtn.forEach((btn) => btn.addEventListener("click", editNote));
-}
-function addCode(){
-    let noteSection = document.querySelector(".note-ul");
-//     let note = document.createElement("div");
-//     // note.classList.add("code-toolbar");
-//     note.innerHTML = `<pre class=" language-py">
-//     <code class="language-py">def main():
-//         print("hello World")
-//     </code>
-// </pre>`;
-    let newNote = noteSection.childNodes[1];
-    let newD = document.createElement("div");
-    newD.innerHTML = `<pre class="line-numbers">
-    <code class="language-python" contenteditable>def main():
-        print("hello man"</code>
-</pre>`;
-    console.log(newD.innerHTML);
-    // Prism.highlight(note.innerHTML, Prism.languages.python, "python");
-    noteSection.appendChild(newD);
-    Prism.highlightAll();
-    // document.querySelectorAll("code")[1].classList.add("language-py");
-    // document.querySelectorAll("pre")[1].classList.add("line-numbers");
-    // console.log(document.querySelectorAll("pre")[1]);
-}
-function showAddLanguage() {
-    overlay.classList.add("active");
-    lightOverlay.classList.remove("active");
-    addLanguagePopUp.classList.remove("hidden");
-    setTimeout(() => {document.addEventListener("click",closePopUps)},500)
-}
-function showDeleteLanguage() {
-    overlay.classList.add("active");
-    lightOverlay.classList.remove("active");
-    deleteLanguagePopUp.classList.remove("hidden");
-    setTimeout(() => {document.addEventListener("click",closePopUps)},500)
-}
-
-
-function editNote(event) {
-    // let parent = event.target.closest(".prettyprint").innerText;
-
-    console.log(event.target);
 }
