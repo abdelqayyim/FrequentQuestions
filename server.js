3bf13a0a6fccc08b3c520f476609296c4e3de6ac
abdelqayyim <abdelymaster@gmail.com>
2022-04-17 19:42:10 -0400

    VERSION 1.0.0
    ABOUT TO DEPLOY TO HEROKU

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*');
